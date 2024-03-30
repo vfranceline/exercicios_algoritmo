2138e49209e0d8c02e97cf699e4ef478c9384328
Vitória Matos <102192886+vfranceline@users.noreply.github.com>
2024-03-30 03:16:17 -0300

    Create problema5.c

---
+++ problema5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  float primeira_av, segunda_av, terceira_av, media;
+  char nome[999];
+
+  scanf("%s", &nome);
+  scanf("%f", &primeira_av);
+  scanf("%f", &segunda_av);
+  scanf("%f", &terceira_av);
+
+  media = (primeira_av*2 + segunda_av*4 + terceira_av*4)/10;
+
+  printf("A MEDIA FINAL DE %s", nome);
+  printf(" FOI: %.2f", media);
+
+  if (media >= 7){
+    printf(" \nAPROVADO");
+  }
+  if (media < 5){
+    printf(" \nREPROVADO");
+  }
+  if (media>=5 && media<7){
+    printf(" \nRECUPERACAO");
+  }
+  
+}
