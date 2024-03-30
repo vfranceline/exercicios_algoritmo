bc992da3d9c98536b787955abae5b1cd4780e6d1
Vitória Matos <102192886+vfranceline@users.noreply.github.com>
2024-03-30 02:24:58 -0300

    Create questao_1.c

---
+++ questao_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  float salario_base, salario_total;
+  
+  scanf("%f", &salario_base);
+  salario_total = (salario_base*1.05)-(salario_base*0.07);
+  printf("%f", salario_total);
+
+}
