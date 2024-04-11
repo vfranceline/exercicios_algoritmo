f16c60550170c6478b9ec173fd471890c27f8dbd
Vitória Matos <102192886+vfranceline@users.noreply.github.com>
2024-04-11 08:23:18 -0300

    Create Q05_ver02.c

---
+++ Q05_ver02.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  float primeira_av, segunda_av, terceira_av, media;
+  char nome[999];
+
+  printf("Insira o seu nome: \n");
+  scanf("%s", &nome);
+  printf("Insira a nota da primeira avaliação: \n");
+  scanf("%f", &primeira_av);
+  printf("Insira a nota da segunda avaliação: \n");
+  scanf("%f", &segunda_av);
+  printf("Insira a nota da terceira avaliação: \n");
+  scanf("%f", &terceira_av);
+
+  media = (primeira_av*2 + segunda_av*4 + terceira_av*4)/10;
+
+  printf("A MEDIA FINAL DE %s", nome);
+  printf(" FOI: %.2f", media);
+
+  if (media >= 7 && media <= 10)
+    printf(" \nAPROVADO");
+ 
+  else{
+      if (media >= 0 && media < 5)
+          printf(" \nREPROVADO");
+      
+      else{
+          if (media >= 5 && media < 7)
+            printf(" \nRECUPERACAO");
+      }
+  }
+}
